3a10036ee06b201994c0fd47b45b821d84349b11
amirreza <amir.rsf1380@gmail.com>
2024-11-03 22:18:35 +0330

    added list and detail view for posts

--- posts/views.py
+++ posts/views.py
@@ -1,1 +1,20 @@
-# Create your views here.
+from django.http import Http404
+from django.views.generic import DetailView, ListView
+
+from .models import Post
+
+
+class PostListView(ListView):
+    model = Post
+    queryset = Post.published.all()
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+    def get(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        if not self.object.published:
+            raise Http404
+        context = self.get_context_data(object=self.object)
+        return self.render_to_response(context)
