514c1871e6271e5df3799cda7085f0b3c3e64ebe
amirreza <amir.rsf1380@gmail.com>
2024-11-03 21:51:08 +0330

    added admin setup for posts

--- posts/admin.py
+++ posts/admin.py
@@ -1,1 +1,13 @@
-# Register your models here.
+from django.contrib import admin
+
+from .forms import PostForm
+from .models import Post
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    form = PostForm
+    list_display = ("title", "status")
+    list_filter = ("status", "created_at", "published_at")
+    search_fields = ("title", "body")
+    ordering = ("-published_at", "status")
