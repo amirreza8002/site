1be166a940ecf66f6a4f7d8adaed813d31c6dc70
amirreza <amir.rsf1380@gmail.com>
2024-11-05 21:47:50 +0330

    tinymce settings

--- django_project/settings.py
+++ django_project/settings.py
@@ -85,6 +85,21 @@ SESSION_CACHE_ALIAS = "default"
 # tailwind
 TAILWIND_APP_NAME = "theme"
 
+# tinymce
+TINYMCE_DEFAULT_CONFIG = {
+    "height": "420px",
+    "width": "960px",
+    "menubar": "file edit view insert format tools table help",
+    "plugins": "advlist autolink lists link image charmap print preview anchor searchreplace visualblocks code "
+    "fullscreen insertdatetime media table paste code help wordcount spellchecker",
+    "toolbar": "undo redo | bold italic underline strikethrough | fontselect fontsizeselect formatselect | alignleft "
+    "aligncenter alignright alignjustify | outdent indent |  numlist bullist checklist | forecolor "
+    "backcolor casechange permanentpen formatpainter removeformat | pagebreak | charmap emoticons | "
+    "fullscreen  preview save print | insertfile image media pageembed template link anchor codesample | "
+    "a11ycheck ltr rtl | showcomments addcomment code",
+    "custom_undo_redo_levels": 10,
+}
+
 
 class Settings(BaseSettings):
     SECRET_KEY = "django-insecure-1xq=$6u(0oy7x3*li2glj6l3$tz#*6jq+je1&cs*uo!89%gba9"
