24e9b0b1d0d6868bd248edd86fcfbd1d3838579a
amirreza <amir.rsf1380@gmail.com>
2024-11-11 15:43:04 +0330

    18n

--- posts/apps.py
+++ posts/apps.py
@@ -1,6 +1,9 @@
 from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
 
 
 class PostsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "posts"
+    verbose_name = _("Post")
+    verbose_name_plural = _("Posts")
