e689c7683b39b2e1a6dca1e610d0c98ee1a81ee7
amirreza <amir.rsf1380@gmail.com>
2024-11-11 15:43:17 +0330

    added pagination

--- posts/views.py
+++ posts/views.py
@@ -7,6 +7,18 @@ from .models import Post
 class PostListView(ListView):
     model = Post
     queryset = Post.published.all()
+    paginate_by = 6
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        paginator = context["paginator"]
+        page = context["page_obj"]
+        if page and paginator:
+            prev_pages_number = page.number - 1
+            next_pages_number = paginator.num_pages - page.number
+            context["prev_pages_number"] = prev_pages_number
+            context["next_pages_number"] = next_pages_number
+        return context
 
 
 class PostDetailView(DetailView):
