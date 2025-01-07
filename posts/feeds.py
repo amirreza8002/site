c59868c4deb8fe11116d29e4c9796c0befa74f34
amirreza <amir.rsf1380@gmail.com>
2025-01-07 13:14:52 +0330

    improve the feed settings

--- posts/feeds.py
+++ posts/feeds.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.contrib.syndication.views import Feed
 from django.template.defaultfilters import truncatewords
 from django.utils.feedgenerator import Atom1Feed
@@ -7,12 +8,12 @@ from .models import Post
 
 class AtomPostFeed(Feed):
     title = "blog"
-    link = "/blog/"
+    link = "/en/blog/"
     subtitle = "Blog posts on my site"
     feed_type = Atom1Feed
 
     def items(self):
-        return Post.published.all()[:100]
+        return Post.published.filter(tags__name__in=("django",))[:100]
 
     def item_title(self, item):
         return item.title
@@ -22,3 +23,14 @@ class AtomPostFeed(Feed):
 
     def item_updateddate(self, item):
         return item.updated_at
+
+    def feed_url(self):
+        if settings.DEBUG:
+            return "http://localhost:8000/en/blog/feed/atom"
+        return "https://teapot.codes/en/blog/feed/atom"
+
+    def author_name(self, obj):
+        return "amirreza sohrabi far"
+
+    def author_email(self, obj):
+        return "amir.rsf1380@gmail.com"
